19d053c2b5e0abdeab0ea6cdaa59f406c88cb5fd
Hansen0314 <595355940@qq.com>
2020-06-01 16:31:38 +0800

    add:example

---
+++ examples/BasicRead.py
@@ -0,0 +1,14 @@
+import seeed_ds18b20
+import time
+
+def main():
+    DS18B20 = seeed_ds18b20.grove_ds18b20()
+    print("Please use Ctrl C to quit")
+    while True:
+        temp_c,temp_f = DS18B20.read_temp
+        print('temp_c %.2f C   temp_f %.2f F' % (temp_c,temp_f),end=" ")
+        print('\r', end='')
+        time.sleep(0.5)
+
+if __name__ == "__main__":
+    main()     
